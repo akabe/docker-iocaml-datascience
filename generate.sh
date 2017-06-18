29fdd92e41cd6da95f47ec748a1a9cdc860063ac
Akinori ABE <aabe.65535@gmail.com>
2017-06-18 14:58:27 +0900

    :sparkles: add ocurl

--- generate.sh
+++ generate.sh
@@ -4,7 +4,19 @@
 function common_scripts() {
     cat <<'EOF'
     (opam install -y batteries || :) && \
-    opam install -y core lacaml slap lbfgs ocephes oml gsl cairo2 archimedes mariadb postgresql && \
+    opam install -y \
+      'core>=v0.9.0' \
+      lacaml \
+      slap \
+      lbfgs \
+      ocephes \
+      oml \
+      gsl \
+      'cairo2>=0.5' \
+      archimedes \
+      'mariadb>=0.8.1' \
+      postgresql \
+      ocurl && \
     \
     find $HOME/.opam -regex '.*\.\(cmt\|cmti\|annot\|byte\)' -delete && \
     rm -rf $HOME/.opam/archives \
@@ -28,7 +40,16 @@ FROM akabe/iocaml:${TAG}
 
 ADD MariaDB.repo /etc/yum.repos.d/MariaDB.repo
 
-RUN sudo yum install -y gfortran blas-devel lapack-devel gsl-devel libffi-devel cairo-devel MariaDB-devel postgresql-devel && \\
+RUN sudo yum install -y \\
+      gfortran \\
+      blas-devel \\
+      lapack-devel \\
+      gsl-devel \\
+      libffi-devel \\
+      cairo-devel \\
+      MariaDB-devel \\
+      postgresql-devel \\
+      libcurl-devel && \\
     sudo ln -sf /usr/lib64/libmysqlclient.so.18.0.0 /usr/lib/libmysqlclient.so && \\
     \\
 $(common_scripts) && \\
@@ -44,7 +65,16 @@ FROM akabe/iocaml:${TAG}
 RUN sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db && \\
     echo 'deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/10.2/debian jessie main' | sudo tee -a /etc/apt/sources.list && \\
     sudo apt-get update && \\
-    sudo apt-get install -y gfortran libffi-dev libblas-dev liblapack-dev libgsl0-dev libcairo2-dev libmariadb-dev libpq-dev && \\
+    sudo apt-get install -y \\
+      gfortran \\
+      libffi-dev \\
+      libblas-dev \\
+      liblapack-dev \\
+      libgsl0-dev \\
+      libcairo2-dev \\
+      libmariadb-dev \\
+      libpq-dev \\
+      libcurl4-openssl-dev && \\
     sudo ln -sf /usr/lib/x86_64-linux-gnu/libmysqlclient.so.20 /usr/lib/libmysqlclient.so && \\
     \\
 $(common_scripts) && \\
